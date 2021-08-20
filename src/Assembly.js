5feca61c7370d3c08cdea3ab8c27a7c837e8110a
gmgercke <gmgercke@zoho.com>
2021-08-20 18:09:19 +0200

    Basic css, dark background, better generation

--- src/Assembly.js
+++ src/Assembly.js
@@ -10,24 +10,28 @@ export default function Assembly() {
     setReady(true);
     setKeyword({
       active: true,
-      mood: prompts[0][Math.floor(Math.random() * prompts[0].length)],
-      race: prompts[1][Math.floor(Math.random() * prompts[1].length)],
-      adjective: prompts[2][Math.floor(Math.random() * prompts[2].length)],
-      wares: prompts[3][Math.floor(Math.random() * prompts[3].length)],
+      build: prompts[0][Math.floor(Math.random() * prompts[0].length)],
+      mood: prompts[1][Math.floor(Math.random() * prompts[1].length)],
+      race: prompts[2][Math.floor(Math.random() * prompts[2].length)],
+      adjective: prompts[3][Math.floor(Math.random() * prompts[3].length)],
+      wares: prompts[4][Math.floor(Math.random() * prompts[4].length)],
     });
   }
 
   if (ready) {
     return (
       <div className="Assembly">
-        <button onClick={generatePrompts}>Hit it!</button>
+        <button className="generate-button" onClick={generatePrompts}>
+          Generate
+        </button>
         <div>
-          Your NPC is a:
+          <span className="text-highlight">Your NPC is a</span>
           <div>
+            <span className="keyword-highlight">{keyword.build}</span>
             <span className="keyword-highlight">{keyword.mood}</span>
             <span className="keyword-highlight">{keyword.race}</span>
           </div>
-          who provides
+          <span className="text-highlight">who provides</span>
           <div>
             <span className="keyword-highlight">{keyword.adjective}</span>
             <span className="keyword-highlight">{keyword.wares}</span>
@@ -38,7 +42,9 @@ export default function Assembly() {
   } else {
     return (
       <div className="Assembly">
-        <button onClick={generatePrompts}>Hit it!</button>
+        <button className="generate-button" onClick={generatePrompts}>
+          Hit it!
+        </button>
       </div>
     );
   }
